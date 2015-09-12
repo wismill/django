9a79e94e882174b0e2cb7f32f086c2cbbc74ecf8
Preston Timmons <prestontimmons@gmail.com>
2015-09-12 11:42:37 -0500

    Fixed #15667 -- Added template-based widget rendering.
    Thanks Carl Meyer and Tim Graham for contributing to the patch.

--- tests/forms_tests/widget_tests/test_selectdatewidget.py
+++ tests/forms_tests/widget_tests/test_selectdatewidget.py
@@ -469,7 +469,7 @@ class SelectDateWidgetTest(WidgetTest):
         )
 
         # Even with an invalid date, the widget should reflect the entered value (#17401).
-        self.assertEqual(w.render('mydate', '2010-02-30').count('selected="selected"'), 3)
+        self.assertEqual(w.render('mydate', '2010-02-30').count('selected'), 3)
 
         # Years before 1900 should work.
         w = SelectDateWidget(years=('1899',))
